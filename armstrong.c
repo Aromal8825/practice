2a408f7b4af47e0a622c9919287a90029486f3fe
aromal <aromalaumesh@gmail.com>
2024-10-17 22:29:39 +0530

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,24 @@
+// check if armstrong or not for 3 digit numbers  //
+#include<stdio.h>
+void main()
+{
+    int num, d, rem, power, sum = 0, temp;
+    printf("enter a number : ");
+    scanf("%d", &num);
+    temp = num;
+    while(num > 0)
+    {
+        rem = num % 10;
+        num = num / 10;
+        power = rem * rem * rem;
+        sum = sum + power;
+    }
+    if( sum == temp)
+    {
+        printf("\n armstrong number ");
+    }
+    else
+    {
+        printf("\n not armstrong number ");
+    }
+}
