c844376973f7d2760788d6dfa09f42a81f1f9dd9
aromal <aromalaumesh@gmail.com>
2024-12-05 19:20:22 +0530

    Create array_search.c

---
+++ array_search.c
@@ -0,0 +1,31 @@
+// search for a given element in array //
+#include<stdio.h>
+void main()
+{
+    int a[10], n, i, search, found;
+    printf("enter number of elements : ");
+    scanf("%d", &n);
+    printf("enter %d numbers : ", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("enter the search element : ");
+    scanf("%d", &search);
+    for (i = 0; i < n; i++)
+    {
+        if(search == a[i])
+        {
+            found = 1;
+            break;
+        }
+    }
+    if(found == 1)
+    {
+        printf("%d found at index %d ", search, i);
+    }
+    else
+    {
+        printf("%d not found ", search);
+    }
+}
